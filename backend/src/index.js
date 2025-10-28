a55ae4e166f2e9513fceb818499f2386ee81b16e
ibrahimaliyevv2 <7caucb@code.edu.az>
2025-10-29 02:47:01 +0400

    fix dirname in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
+// 1. Import fileURLToPath
+import { fileURLToPath } from 'url';
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,7 +17,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// 2. Redefine __dirname robustly
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,9 +37,10 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get(/.*/, (req, res) => {
+  // Route now uses RegExp
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
+  });
 }
 
 server.listen(PORT, () => {
